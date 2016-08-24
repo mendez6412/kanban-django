fa7b9f0b204e0ca4829d93fe7965beb45672664c
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-08-24 02:07:17 -0400

    Added login/logout/registration views, forms, templates

--- kanban/hanabi/urls.py
+++ kanban/hanabi/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^boards/$', views.boards, name='boards')
+    url(r'^boards/$', views.boards, name='boards'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.signin, name='login'),
+    url(r'^logout/$', views.signout, name='logout'),
 ]
--- kanban/hanabi/views.py
+++ kanban/hanabi/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board, Status, Task, User
 from .serializers import BoardSerializer
 from rest_framework import viewsets
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
@@ -10,6 +14,38 @@ def index(request):
 
 def boards(request):
     return render(request, 'hanabi/boards.html')
+
+
+def signin(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+    else:
+        return render(request, 'hanabi/login.html')
+
+
+def signout(request):
+    logout(request)
+    return render(request, 'hanabi/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST, prefix='user')
+        if user_form.is_valid():
+            user = user_form.save(commit=False)
+            user.save()
+            return HttpResponseRedirect('/')
+    else:
+        user_form = UserCreationForm(prefix='user')
+    context = {
+        'userform': user_form
+    }
+    return render(request, 'hanabi/register.html', context)
 
 
 class BoardViewSet(viewsets.ModelViewSet):
